6a678abefb00203058ed0a622949b79af2c03058
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2019-07-24 11:21:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,34 +17,23 @@ menu          = True
 
 def getWebpage(champion):
    url = 'https://rankedboost.com/league-of-legends/counter/' + champion
-
    page = requests.get(url)
-
    getWinPlayBanRate(page)
-
    return getCounterChampions(page)
 
 def getCounterChampions(page):
      champList = []
-
      soup = BeautifulSoup(page.text, 'html.parser')
-
      counterChampions = soup.find_all(class_='RecommendedCounterName', string = True)
-
      for champion in counterChampions:
        champList.append(champion.findAll(text = True))
-
      champList = [x[0] for x in champList]
-
      return formatChampList(champList)
 
 def getWinPlayBanRate(page):
      soup = BeautifulSoup(page.text, 'html.parser')
-
      stats = soup.find_all(class_='StatsTD', string = True)
-
      print(champName + " has a " + stats[0].text + " winrate, " + stats[1].text + " pick rate, " + stats[2].text + " ban rate.")
-
 
 def formatChampList(champList):
     strongAgainst = []
@@ -57,7 +46,6 @@ def formatChampList(champList):
       else:
         strongAgainst.append(x)
         counter += 1
-
     print(champName + " is Strong against these champions: " + ", ".join(strongAgainst))
     print(champName + " is Weak against these champions: " + ", ".join(weakAgainst))
     print()
